a2497833d6624fa7b145bba2b7c49afd357ac0c3
iceberg53 <chabi2353@gmail.com>
2025-03-03 00:41:33 +0100

    Updates

--- assets/index-KtmTkqfe.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-8By47L5I.js";import{M as e}from"./Meta-zxqktxub.js";import{F as t,T as a}from"./App-_BqqGbRn.js";import"./index-Ed2Csewq.js";import"./index.esm-OiWKcXSR.js";function p(){return r.jsxs(r.Fragment,{children:[r.jsx(e,{title:"page 3"}),r.jsx(t,{children:r.jsx(a,{variant:"h3",children:"Page 3"})})]})}export{p as default};
